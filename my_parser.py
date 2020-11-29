b15983e328fd207950e998060b4e2abc2ca3476f
Rainory <gusev.ea@phystech.edu>
2020-11-29 16:14:23 +0300

    Update my_parser.py
    Переписал через requsets, должно работать

--- my_parser.py
+++ my_parser.py
@@ -1,10 +1,13 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
+import requests
 
-
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
 def pars(data, res):
     for i in range(len(data)):
@@ -19,11 +22,8 @@ def pars(data, res):
 def get_today():
     # Находим акции с хорошим потенциалом роста, которые просели за последней месяц
     # парсим их с сайта www.finviz.com
-    browser = webdriver.Chrome(chrome_options=options)
     url = 'https://www.finviz.com/screener.ashx?v=111&f=cap_midover,fa_debteq_u1,fa_eps5years_o15,fa_pe_u15,ta_perf_4w10u&o=pe'
-    browser.get(url)
-    html = browser.page_source
-    browser.close()
+    html = requests.get(url, headers=hdr).text
     soup = BeautifulSoup(html, 'html.parser')
     data = soup.find_all('tr', {'class': 'table-dark-row-cp'})
     res = []
@@ -34,10 +34,7 @@ def get_today():
 
 def price(s):
     #получаем текущую цену акции
-    browser = webdriver.Chrome(chrome_options=options)
-    browser.get(s)
-    html = browser.page_source
-    browser.close()
+    html = requests.get(s, headers=hdr).text
     soup = BeautifulSoup(html, 'html.parser')
     data = soup.find('table', {'class': 'snapshot-table2'})
     data = data.find_all('tr')[10]
